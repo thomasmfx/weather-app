6801241b0924fadd2c4da3871a9da12efd6cd5a0
Thomas Moisés <161066238+thomasmfx@users.noreply.github.com>
2024-07-11 20:03:33 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Try running 'npx eslint ./src/index.js' to test ESLint
+
+var a = 1; var b = 2;
+function foo ( ) {console . log( a+b )}
+foo  ( )
+if( true ) { console.log ( 'Teste ESLint' );}
+var obj={key:"valor"};console .log(obj.key)
+for(var i=0;i<10;i++){console.log(i)}
+function bar() {return "Teste"}
+console.log(bar( ))
