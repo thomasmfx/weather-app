2c6b0ae405e9267202e291551f5aacfd4d6a99d6
thomasmfx <thomasmoisesf@gmail.com>
2024-07-14 21:32:05 -0300

    feat/refactor: Separate DOM from logic and refactor code to better organization

---
+++ src/dom.js
@@ -0,0 +1,125 @@
+import {
+  fetchWeather,
+  toggleTemperature,
+  getStoredData
+} from "./index.js";
+
+const form = document.querySelector('form');
+const errorMsg = document.querySelector('#error-message-container');
+const closeErrorMsg = document.querySelector('#close-error-message');
+const toggleTemperatureBtns = document.querySelectorAll('.temperature');
+const dropdownBtn = document.querySelector('#dropdown-btn');
+const content = document.querySelector('#dropdown-content');
+
+function getLocationValue() {
+  const input = document.querySelector('#search-bar');
+  const locationName = input.value;
+
+  if (locationName !== '') {
+    input.value = '';
+    return locationName;
+  };
+
+  return null;
+};
+
+function displayWeather() {
+  let weather = getStoredData();
+  let tempPreference;
+
+  if(localStorage.getItem('temp')) {
+    tempPreference = JSON.parse(localStorage.getItem('temp'));
+  } else {
+    tempPreference = 'c';
+  };
+
+  if (weather){
+    document.querySelector('#main-weather-condition')
+    .textContent = weather.condition;
+    document.querySelector('#temp')
+    .textContent = weather.temp[`${tempPreference}`]
+    document.querySelector('#current-location')
+    .textContent = weather.location;
+    document.querySelector('#localdate')
+    .textContent = weather.localDate;
+    document.querySelector('#mintemp')
+    .textContent = weather.minTemp[`${tempPreference}`];
+    document.querySelector('#maxtemp')
+    .textContent = weather.maxTemp[`${tempPreference}`];
+    document.querySelector('#humidity')
+    .textContent = weather.humidity;
+    document.querySelector('#wind')
+    .textContent = weather.wind;
+    document.querySelector('#tempicon')
+    .src = `../assets/1st Set - Monochrome/${weather.icon}.svg`;
+    updateBackgroundColor(weather);
+  };
+};
+
+function updateBackgroundColor(weather){
+  const body = document.querySelector('body');
+  if (weather.icon.includes('day')) {
+    body.style.background = 'var(--day)';
+  } else if (weather.icon.includes('night')) {
+    body.style.background = 'var(--night)';
+  } else if (weather.icon.includes('snow')) {
+    body.style.background = 'var(--snow)';
+  } else if (
+    weather.icon.includes('overcast')
+    || weather.icon.includes('cloudy')
+  ) {
+    body.style.background = 'var(--overcast)';
+  } else if (weather.icon.includes('rain')) { 
+    body.style.background = 'var(--rain)';
+  } else {
+    body.style.background = 'var(--default)';
+  };
+};
+
+function updateTemperatureInDOM(temp) {
+  const button = document.querySelectorAll('.temperature');
+  button.forEach((btn) => {
+    if(btn.id === temp) {
+      btn.firstElementChild.className = 'stripe';
+    } else {
+      btn.firstElementChild.className = ''
+    };
+  });
+};
+
+function hideLoader(){
+  const loader = document.querySelector('#loader-container');
+  loader.style.visibility = 'hidden';
+};
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const weather = await fetchWeather(getLocationValue());
+  displayWeather(weather);
+});
+
+closeErrorMsg.addEventListener('click', () => {
+  errorMsg.style.visibility = 'hidden';
+  errorMsg.style.display = '';
+});
+
+dropdownBtn.addEventListener('click', () => {
+  if (content.style.visibility === 'visible') {
+    content.style.visibility = 'hidden';
+  } else {
+    content.style.visibility = 'visible';
+  };
+});
+
+toggleTemperatureBtns.forEach((button) => {
+  button.addEventListener('click', () => {
+    toggleTemperature(button.id)
+  });
+});
+
+export {
+  displayWeather,
+  hideLoader,
+  errorMsg,
+  updateTemperatureInDOM
+}
--- src/index.js
+++ src/index.js
@@ -1,179 +1,133 @@
-const form = document.querySelector('form');
-const errorMsg = document.querySelector('#error-message-container');
-const closeErrorMsg = document.querySelector('#close-error-message');
-const toggleTempBtns = document.querySelectorAll('.temperature');
-let userTempPreference = 'tempC';
+import { 
+  displayWeather,
+  hideLoader,
+  errorMsg,
+  updateTemperatureInDOM
+} from "./dom.js";
 
-function getLocationValue() {
-  const input = document.querySelector('#search-bar');
-  const locationName = input.value;
+function formatData(data){
+  const dataObj = {};
 
-  if (locationName !== '') {
-    input.value = '';
-    return locationName;
+  dataObj.condition = data.currentConditions.conditions;
+  dataObj.location = data.resolvedAddress;
+  dataObj.localDate = data.days[0].datetime;
+  dataObj.humidity = data.days[0].humidity;
+  dataObj.wind = data.days[0].windspeed;
+  dataObj.icon = data.currentConditions.icon;
+  dataObj.temp = {
+    c: data.currentConditions.temp,
+    f: Math.round(((data.currentConditions.temp * 1.8) + 32)),
+    k: (data.currentConditions.temp + 273)
+  };
+  dataObj.minTemp = {
+    c: data.days[0].tempmin,
+    f: Math.round(((data.days[0].tempmin * 1.8) + 32)),
+    k: (data.days[0].tempmin + 273)
+  };
+  dataObj.maxTemp = {
+    c: data.days[0].tempmax,
+    f: Math.round(((data.days[0].tempmax * 1.8) + 32)),
+    k: (data.days[0].tempmax + 273)
   };
 
-  return null;
+  return dataObj;
+}
+
+function storeData(dataName, data) {
+  localStorage.setItem(`${dataName}`, JSON.stringify(data));
 };
 
-function storeWeather(weatherObj) {
-  localStorage.setItem('weather', JSON.stringify(weatherObj));
+function getStoredData(){
+  return JSON.parse(localStorage.getItem('weather'));
+};
+
+function onSucces(dataName, data){
+  storeData(dataName, formatData(data));
+};
+
+function onError(){
+  errorMsg.style.visibility = 'visible';
+  errorMsg.style.display = '';
 };
 
 async function fetchWeather(location){
-  const weather = {};
-  
   try {
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=FK7J8WFSF4RXPK7GLDSQ78P6S&contentType=json`,
        { mode: 'cors' }
     );
     const data = await response.json();
-    
-    weather.condition = data.currentConditions.conditions;
-    weather.tempC = data.currentConditions.temp;
-    weather.tempK = (weather.tempC + 273);
-    weather.tempF = ((weather.tempC * 1.8) + 32);
-    weather.location = data.resolvedAddress;
-    weather.localDate = data.days[0].datetime;
-    weather.minTemp = data.days[0].tempmin;
-    weather.maxTemp = data.days[0].tempmax;
-    weather.humidity = data.days[0].humidity;
-    weather.wind = data.days[0].windspeed;
-    weather.icon = data.currentConditions.icon;
-    storeWeather(weather)
+    onSucces('weather', data);
   } catch (error) {
-    errorMsg.style.visibility = 'visible';
-    errorMsg.style.display = '';
-    return null;
-  };
-
-  return weather;
-};
-
-function updateBackgroundColor(weather){
-  const body = document.querySelector('body');
-  if (weather.icon.includes('day')) {
-    body.style.background = 'var(--day)';
-  } else if (weather.icon.includes('night')) {
-    body.style.background = 'var(--night)';
-  } else if (weather.icon.includes('snow')) {
-    body.style.background = 'var(--snow)';
-  } else if (
-    weather.icon.includes('overcast')
-    || weather.icon.includes('cloudy')
-  ) {
-    body.style.background = 'var(--overcast)';
-  } else if (weather.icon.includes('rain')) { 
-    body.style.background = 'var(--rain)';
-  } else {
-    body.style.background = 'var(--default)';
+    return onError();
   };
 };
 
-function displayWeather(weatherObj) {
-  if (weatherObj){
-    document.querySelector('#main-weather-condition')
-    .textContent = weatherObj.condition;
-    document.querySelector('#temp')
-    .textContent = weatherObj.tempC;
-    document.querySelector('#current-location')
-    .textContent = weatherObj.location;
-    document.querySelector('#localdate')
-    .textContent = weatherObj.localDate;
-    document.querySelector('#mintemp')
-    .textContent = weatherObj.minTemp;
-    document.querySelector('#maxtemp')
-    .textContent = weatherObj.maxTemp;
-    document.querySelector('#humidity')
-    .textContent = weatherObj.humidity;
-    document.querySelector('#wind')
-    .textContent = weatherObj.wind;
-    document.querySelector('#tempicon')
-    .src = `../assets/1st Set - Monochrome/${weatherObj.icon}.svg`;
-    updateBackgroundColor(weatherObj);
-  };
+function storeTemperature(temp){
+  localStorage.setItem('temp', JSON.stringify(temp));
 };
 
-form.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  const weather = await fetchWeather(getLocationValue());
-  displayWeather(weather);
-});
-
-closeErrorMsg.addEventListener('click', () => {
-  errorMsg.style.visibility = 'hidden';
-  errorMsg.style.display = '';
-});
-
-const dropdownBtn = document.querySelector('#dropdown-btn');
-const content = document.querySelector('#dropdown-content');
-
-dropdownBtn.addEventListener('click', () => {
-  if (content.style.visibility === 'visible') {
-    content.style.visibility = 'hidden';
-  } else {
-    content.style.visibility = 'visible';
-  };
-});
-
-
-function changeTempPreference(button, value){
-  toggleTempBtns.forEach((btn) => {
-    btn.firstElementChild.className = ''
-  })
-
-  button.firstElementChild.className = 'stripe';
-  userTempPreference = value;
-  localStorage.setItem('tempPreference', JSON.stringify(userTempPreference));
-};
-
-
-function updateUserTempPreference(){
-  const currentTempPreference = JSON.parse(localStorage.getItem('tempPreference'));
-  const currentWeather = JSON.parse(localStorage.getItem('weather'));
-  if(currentTempPreference){
-    document.querySelector('#temp')
-    .textContent = currentWeather[`${currentTempPreference}`];
-  };
-  
-  document.querySelector(`#${currentTempPreference}`)
-  .firstElementChild
-  .className = 'stripe';
-};
-
-toggleTempBtns.forEach((button) => {
-  button.addEventListener('click', () => {
-    changeTempPreference(button, button.id);
-    updateUserTempPreference();
-  });
-});
-
-function hideLoader(){
-  const loader = document.querySelector('#loader-container');
-  loader.style.visibility = 'hidden';
+function toggleTemperature(newTemp){
+  storeTemperature(newTemp);
+  updateTemperatureInDOM(newTemp);
+  displayWeather();
 };
 
 async function firstLoad(){
-  if(localStorage.getItem('weather')){
-    const lastLocation = JSON.parse(localStorage.getItem('weather'));
-    const data = await fetchWeather(lastLocation.location);
-    displayWeather(data);
+  const storedTemp = JSON.parse(localStorage.getItem('temp'));
+  const storedWeather = JSON.parse(localStorage.getItem('weather'));
+
+  if(storedTemp){
+    updateTemperatureInDOM(storedTemp);
+  } else {
+    toggleTemperature('c');
+  };
+
+  if(storedWeather){
+    await fetchWeather(storedWeather.location);
+    displayWeather();
+
     setTimeout(() => {
       hideLoader();
     }, 2000);
   } else {
-    const data = await fetchWeather('são paulo');
-    displayWeather(data)
+    await fetchWeather('são paulo');
+    // localStorage.setItem('weather', JSON.stringify(temporary))
+    displayWeather()
+
     setTimeout(() => {
       hideLoader();
     }, 2000);
   };
-
-  if(!localStorage.getItem('tempPreference')){
-    localStorage.setItem('tempPreference', JSON.stringify(userTempPreference));
-  };
-
-  updateUserTempPreference();
 };
 
 window.onload = firstLoad();
+
+export {
+  fetchWeather,
+  toggleTemperature,
+  getStoredData
+}
+
+// let temporary = {
+//   "condition": "Overcast",
+//   "temp": {
+//     "c": 13.9,
+//     "f": Math.round(57.019999999999996),
+//     "k": 286.9
+//   },
+//   "location": "Guaianazes, São Paulo, SP, Brasil",
+//   "localDate": "2024-07-13",
+//   "minTemp": {
+//     c: 13.3,
+//     f: Math.round(((13.3 * 1.8) + 32)),
+//     k: (13.3 + 273)
+//   },
+//   "maxTemp": {
+//     c: 17.2,
+//     f: Math.round(((17.2 * 1.8) + 32)),
+//     k: (17.2 + 273)
+//   },
+//   "humidity": 88.8,
+//   "wind": 14.4,
+//   "icon": "cloudy"
+// }
